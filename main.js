7d080470fc955c9fcdf448a98c53847b5aa2f58d
Dercont <armando.almengor@utp.ac.pa>
2020-06-01 13:28:09 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//1. Declarar dos variables con la palabra reservada "var", imprimir el resultado de las operaciones de "Suma", "Resta", "Multiplicación" y "División" en la consola. Las variables pueden ser estáticas.
+//2. Declarar dos variables con la palabra reservada "let", imprimir la concatenación de ellas.
+//3. Declarar dos variables con la palabra reservada "const", imprimir cuál es el tipo de dato de esa variable.
+//4. Declarar una variable tipo Objecto, colocar 4 llaves dentro de él que contengan un entero, un string, un booleano y un objeto vacío, en ese mismo orden.
+
